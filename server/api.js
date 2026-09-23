90f2e5d7ec685e97f8867bca2398ffcc369eb65d
Pinkerton3 <dep3@speedpost.net>
2026-09-23 09:39:44 -0400

    Finish minion routes

--- server/api.js
+++ server/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllFromDatabase, addToDatabase, getFromDatabaseById } = require('./db');
+const { getAllFromDatabase, addToDatabase, getFromDatabaseById, updateInstanceInDatabase } = require('./db');
 const apiRouter = express.Router();
 const allMinions = getAllFromDatabase('minions');
 //console.log(allMinions);
@@ -7,6 +7,8 @@ const allMinions = getAllFromDatabase('minions');
 
 
 module.exports = apiRouter;
+
+// Minion routes
 
 // GET all minions
 apiRouter.get('/minions', (req, res, next) => {
@@ -18,6 +20,7 @@ apiRouter.post('/minions', (req, res, next) => {
   const newMinion = addToDatabase('minions', req.body);
   if(newMinion) {
     allMinions.push(newMinion);
+    addToDatabase('minions', newMinion);
     res.status(201).send(newMinion);
   } else {
     res.status(400).send();
@@ -36,10 +39,46 @@ apiRouter.get('/minions/:minionId', (req, res, next) => {
 
 // PUT single minion by id
 apiRouter.put('/minions/:minionId', (req, res, next) => {
-  res.status(400).send();
+  const minionId = getFromDatabaseById('minions', req.params.minionId);
+  if (minionId) {
+    const updatedMinion = Object.assign(minionId, req.body);
+    updateInstanceInDatabase('minions', updatedMinion);
+  } else {
+    res.status(404).send();
+  }
 })
 
 // DELETE single minion by id
 apiRouter.delete('/minions/:minionId', (req, res, next) => {
-  res.status(400).send();
+  const minionId = getFromDatabaseById('minions', req.params.minionId);
+  if (minionId) {
+    deleteFromDatabasebyId('minions', req.params.minionId);
+    res.status(204).send();
+  } else {
+    res.status(400).send();
+  }
 })
+
+// Idea routes
+
+// Get all ideas
+
+apiRouter.get('/ideas', (req, res, next) => {
+  const allIdeas = getFromDatabase('ideas');
+  res.send(allIdeas);
+})
+
+// Post new idea
+
+apiRouter.post('/ideas', (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body);
+  if(newIdea) {
+    
+  }
+})
+
+// Get single idea
+
+// Put single idea
+
+// Delete single idea
